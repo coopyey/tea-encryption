a044b47953eae773db5ef61ccff0dd74b850d82e
Coop <mjmohr1@shockers.wichita.edu>
2018-03-26 19:40:14 -0500

    input file and file reading fixed

--- code/inout.h
+++ code/inout.h
@@ -5,9 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-void input_64(char in[]);
+void input_64(char *in);
 void input_512(char *in);
-//void input_4096(char in[]);
-//void input_32768(char in[]);
+void input_4096(char *in);
+void input_32768(char *in);
 
 #endif
--- code/main.c
+++ code/main.c
@@ -7,8 +7,8 @@
 int main() {
     char in_64[65];
     char in_512[513];
-    //char in_4096[4097];
-    //char in_32768[32769];
+    char in_4096[4097];
+    char in_32768[32769];
 
     input_64(in_64);
     printf("%s\n\n", in_64);
@@ -16,11 +16,11 @@ int main() {
     input_512(in_512);
     printf("%s\n\n", in_512);
 
-    /*input_4096(in_4096);
-    //printf("%s\n\n", in_4096);
+    input_4096(in_4096);
+    printf("%s\n\n", in_4096);
 
     input_32768(in_32768);
-    //printf("%s\n", in_32768); */
+    printf("%s\n", in_32768); 
 
     return 0;
 }
