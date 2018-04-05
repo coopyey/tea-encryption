9a469fcb60892da47bc35456439c97d81b89d0b0
Coop <mjmohr1@shockers.wichita.edu>
2018-04-05 08:26:14 -0500

    added notes for clarity

--- code/crypto.c
+++ code/crypto.c
@@ -1,5 +1,6 @@
 #include "crypto.h"
 
+//Function generates a random stream of bytes for use
 char *rand_stream(size_t num) {
   char *stream = malloc(num);
   size_t i;
@@ -10,6 +11,7 @@ char *rand_stream(size_t num) {
   return stream;
 }
 
+//TEA ofc works on 64 bits at a time - modiified to uint32_t from unsigned long for portability
 void tea_encrypt(uint32_t *const v,uint32_t *const w,const uint32_t *const k) {
    register uint32_t y=v[0],z=v[1],sum=0,delta=0x9E3779B9,a=k[0],b=k[1],c=k[2],d=k[3],n=32;
 
@@ -22,8 +24,7 @@ void tea_encrypt(uint32_t *const v,uint32_t *const w,const uint32_t *const k) {
    w[0]=y; w[1]=z;
 }
 
-void tea_decrypt(uint32_t *const v,uint32_t *const w,const uint32_t *const k)
-{
+void tea_decrypt(uint32_t *const v,uint32_t *const w,const uint32_t *const k) {
    register uint32_t y=v[0],z=v[1],sum=0xC6EF3720,delta=0x9E3779B9,a=k[0],b=k[1],c=k[2],d=k[3],n=32;
 
    /* sum = delta<<5, in general sum = delta * n */
@@ -37,6 +38,7 @@ void tea_decrypt(uint32_t *const v,uint32_t *const w,const uint32_t *const k)
    w[0]=y; w[1]=z;
 }
 
+//DES CBC en/decryption using the openssl library
 void des_cbc(char *in) {
   int length = strlen(in)+1;
   unsigned char out[length], back[length];
@@ -86,6 +88,7 @@ void des_cbc(char *in) {
   }
 }
 
+//DES OFB en/decryption using the openssl library
 void des_ofb(char *in) {
   int length = strlen(in)+1;
   unsigned char out[length], back[length];
@@ -134,9 +137,10 @@ void des_ofb(char *in) {
   }
 }
 
+//TEA CBC implementation
 void tea_cbc_mode(char *in) {
-  int length = strlen(in)+1;
-  int rounds = length/8;
+  int length = strlen(in)+1; //total length of input +1
+  int rounds = length/8; //number of blocks to process based on input
   int i;
   char key[16], ivsetup[8], ivec[8], cur_block[8], returned[8];
   char ctext[length], ptext[length], temp[length];
@@ -152,6 +156,7 @@ void tea_cbc_mode(char *in) {
   memset(temp, 0, sizeof(temp));
   memset(ivec, 0, sizeof(ivec));
 
+  //initialization
   memcpy(key,rand_stream(16),16);
   memcpy(ivsetup,rand_stream(8),8);
   strcpy(temp,in);
@@ -161,13 +166,15 @@ void tea_cbc_mode(char *in) {
   gettimeofday(&start,NULL);
   for (i=0; i<rounds; i++) {
     if(i==0) {
-      memcpy(cur_block,temp,8);
+      memcpy(cur_block,temp,8); //set current working block
 
+      //XOR working block with iv
       int j;
       for (j=0; j<=8; j++) {
         cur_block[j] ^= ivec[j];
       }
      
+      //encrypt current block + update ctext and iv
       tea_encrypt((uint32_t*)cur_block,(uint32_t*)returned,(uint32_t*)key);
 
       memcpy(ivec,returned,8);
@@ -175,17 +182,20 @@ void tea_cbc_mode(char *in) {
     } else {
         int pos=(i*8)+1; //set to start pos
         int itr = 0;
+        //Set current working block based on round number
         while (itr<=8) {
           cur_block[itr] = temp[pos];
           pos++;
           itr++;
         }
 
+        //XOR current block with iv
         int j;
         for (j=0; j<=8; j++) {
           cur_block[j] ^= ivec[j];
          }
 
+      //encrypt current block + update iv and append ctext
       tea_encrypt((uint32_t*)cur_block,(uint32_t*)returned,(uint32_t*)key);
 
       memcpy(ivec,returned,8);
@@ -195,6 +205,7 @@ void tea_cbc_mode(char *in) {
   gettimeofday(&end,NULL);
   printf("%d TEA CBC encrypt took %li useconds.\n",length-1,(end.tv_usec-start.tv_usec));
 
+  //output bc i was too lazy to realloc and the like
   if((length-1)==64) {
     output_64(ctext,'c','2');
   } else if ((length-1)==512) {
@@ -205,6 +216,7 @@ void tea_cbc_mode(char *in) {
     output_32768(ctext,'c','2');
   }
 
+  //reinitialize values
   memset(cur_block, 0, 8);
   memset(returned, 0, 8);
   memcpy(ivec, ivsetup, 8);
@@ -213,11 +225,13 @@ void tea_cbc_mode(char *in) {
   gettimeofday(&start,NULL);
   for (i=0; i<rounds; i++) {
     if(i==0) {
-      memcpy(temp,ctext,length);
-      memcpy(cur_block,temp,8);
+      memcpy(temp,ctext,length); //set working string to ciphertext
+      memcpy(cur_block,temp,8); //set working block
 
+      //"decrypt" ctext
       tea_decrypt((uint32_t*)cur_block,(uint32_t*)returned,(uint32_t*)key);
 
+      //XOR "decrypted" text with iv + update iv and set ptext
       int h;
       for (h=0; h<=8; h++) {
         returned[h] ^= ivec[h];
@@ -228,14 +242,17 @@ void tea_cbc_mode(char *in) {
     } else {
         int pos=(i*8)+1; //set to start pos
         int itr = 0;
+        //set work block based on round number
         while (itr<=8) {
           cur_block[itr] = temp[pos];
           pos++;
           itr++;
         }
 
+        //"decrypt" ptext
         tea_decrypt((uint32_t*)cur_block,(uint32_t*)returned,(uint32_t*)key);
 
+        //XOR "decrypted" text with iv + update iv and append ptext
         int h;
         for(h=0; h<=8; h++) {
           returned[h] ^= ivec[h];
@@ -248,6 +265,7 @@ void tea_cbc_mode(char *in) {
   gettimeofday(&end,NULL);
   printf("%d TEA CBC decrypt took %li useconds.\n",length-1,(end.tv_usec-start.tv_usec));
 
+  //output bc i was too lazy to realloc and the like
   if((length-1)==64) {
     output_64(ptext,'p','2');
   } else if ((length-1)==512) {
@@ -259,9 +277,10 @@ void tea_cbc_mode(char *in) {
   }
 }
 
+//TEA OFB implementation
 void tea_ofb_mode(char *in) {
-  int length = strlen(in)+1;
-  int rounds = length/8;
+  int length = strlen(in)+1; //total length of input +1
+  int rounds = length/8; //number of blocks to process based on input
   int i;
   char key[16], nonceset[8], nonceec[8], cur_block[8], returned[8];
   char ctext[length], ptext[length], temp[length];
@@ -288,38 +307,46 @@ void tea_ofb_mode(char *in) {
     if(i==0) {
       memcpy(cur_block,temp,8);
      
+      //sets new iv value based on encrypted nonce
       tea_encrypt((uint32_t*)nonceec,(uint32_t*)returned,(uint32_t*)key);
       memcpy(nonceec,returned,8);
 
+      //XOR working block with updated nonce
       int j;
       for (j=0; j<=8; j++) {
         cur_block[j] ^= returned[j];
       }
 
+      //sets first 64 bits of ciphertext to encrypted value
       memcpy(ctext,cur_block,8);
     } else {
       int pos=(i*8)+1; //set to start pos
       int itr = 0;
+      //sets working block according to round
       while (itr<8) {
         cur_block[itr] = temp[pos];
         pos++;
         itr++;
       }
 
+      //sets new iv balue based on encrypted value
       tea_encrypt((uint32_t*)nonceec,(uint32_t*)returned,(uint32_t*)key);
       memcpy(nonceec,returned,8);
 
+      //XOR working block with updated nonce
       int j;
       for (j=0; j<=8; j++) {
         cur_block[j] ^= returned[j];
        }
 
+      //appends encrypted block to ciphertext
       memcpy(ctext+(i*8),cur_block,8);
     } //end if/else
   }//end for i
   gettimeofday(&end,NULL);
   printf("%d TEA OFB encrypt took %li useconds.\n",length-1,(end.tv_usec-start.tv_usec));
 
+  //output bc i was too lazy to realloc and the like
   if((length-1)==64) {
     output_64(ctext,'c','3');
   } else if ((length-1)==512) {
@@ -330,6 +357,7 @@ void tea_ofb_mode(char *in) {
     output_32768(ctext,'c','3');
   }
 
+  //clear/reset values
   memset(cur_block, 0, 8);
   memset(returned, 0, 8);
   memcpy(nonceec, nonceset, 8);
@@ -338,29 +366,36 @@ void tea_ofb_mode(char *in) {
   gettimeofday(&start,NULL);
   for (i=0; i<rounds; i++) {
     if(i==0) {
+      //take in ciphertext as new working string & set first working block
       memcpy(temp,ctext,length);
       memcpy(cur_block,temp,8);
 
+      //update new iv with encrypted nonce
       tea_encrypt((uint32_t*)nonceec,(uint32_t*)returned,(uint32_t*)key);
       memcpy(nonceec,returned,8);
 
+      //XOR working block with nonce + set plaintext start
       int h;
       for (h=0; h<=8; h++) {
         cur_block[h] ^= returned[h];
       }
+
       memcpy(ptext,cur_block,8);
     } else {
       int pos=(i*8)+1; //set to start pos
       int itr = 0;
+      //set current working block based on round #
       while (itr<8) {
         cur_block[itr] = temp[pos];
         pos++;
         itr++;
       }
 
+      //encrypte iv and update
       tea_encrypt((uint32_t*)cur_block,(uint32_t*)returned,(uint32_t*)key);
       memcpy(nonceec,returned,8);
 
+      //XOR current working block with updated nonce + append ptext
       int h;
       for(h=0; h<=8; h++) {
         cur_block[h] ^= returned[h];
@@ -372,6 +407,7 @@ void tea_ofb_mode(char *in) {
   gettimeofday(&end,NULL);
   printf("%d TEA OFB decrypt took %li useconds.\n",length-1,(end.tv_usec-start.tv_usec));
 
+  //output block bc i was too lazy to realloc and the like
   if((length-1)==64) {
     output_64(ptext,'p','3');
   } else if ((length-1)==512) {
